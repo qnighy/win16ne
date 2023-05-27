f6d0c8dbbcad43199ec8301ed06b881d16d79426
Masaki Hara <ackie.h.gmai@gmail.com>
2023-05-27 11:13:58 +0900

    Add MZ test

--- src/mz.rs
+++ src/mz.rs
@@ -111,3 +111,120 @@ impl DosHeader {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::io::Cursor;
+
+    use super::*;
+
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+    pub struct DosHeader2 {
+        /// MZ Header signature
+        pub magic: u16,
+        /// Bytes on last page of file
+        pub cblp: u16,
+        /// Pages in file
+        pub cp: u16,
+        /// Relocations
+        pub crlc: u16,
+        /// Size of header in paragraphs
+        pub cparhdr: u16,
+        /// Minimum extra paragraphs needed
+        pub minalloc: u16,
+        /// Maximum extra paragraphs needed
+        pub maxalloc: u16,
+        /// Initial (relative) SS value
+        pub ss: u16,
+        /// Initial SP value
+        pub sp: u16,
+        /// Checksum
+        pub csum: u16,
+        /// Initial IP value
+        pub ip: u16,
+        /// Initial (relative) CS value
+        pub cs: u16,
+        /// File address of relocation table
+        pub lfarlc: u16,
+        /// Overlay number
+        pub ovno: u16,
+        /// Reserved words
+        pub res: [u16; 4],
+        /// OEM identifier (for e_oeminfo)
+        pub oemid: u16,
+        /// OEM information; e_oemid specific
+        pub oeminfo: u16,
+        /// Reserved words
+        pub res2: [u16; 10],
+        /// Offset to extended header
+        pub lfanew: u32,
+    }
+
+    impl From<DosHeader> for DosHeader2 {
+        fn from(h: DosHeader) -> Self {
+            DosHeader2 {
+                magic: h.magic,
+                cblp: h.cblp,
+                cp: h.cp,
+                crlc: h.crlc,
+                cparhdr: h.cparhdr,
+                minalloc: h.minalloc,
+                maxalloc: h.maxalloc,
+                ss: h.ss,
+                sp: h.sp,
+                csum: h.csum,
+                ip: h.ip,
+                cs: h.cs,
+                lfarlc: h.lfarlc,
+                ovno: h.ovno,
+                res: h.res,
+                oemid: h.oemid,
+                oeminfo: h.oeminfo,
+                res2: h.res2,
+                lfanew: h.lfanew,
+            }
+        }
+    }
+
+    #[test]
+    fn test_dos_header_size() {
+        assert_eq!(std::mem::size_of::<DosHeader>(), 0x40);
+    }
+
+    #[test]
+    fn test_dos_header() {
+        let buf: [u8; 0x40] = *b"\
+            MZ\xD4\x01\x06\x00\x00\x00\x20\x00\x00\x00\xFF\xFF\x00\x00\
+            \x00\x00\x3D\x98\x00\x00\x00\x00\x40\x00\x00\x00\x01\x00\x00\x00\
+            \x00\x00\x00\x00\x00\x10\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\
+            \x00\x00\x00\x00\x00\x10\x00\x00\x00\x00\x00\x00\x00\x06\x00\x00\
+        ";
+        let h = DosHeader::read(&mut Cursor::new(buf)).unwrap();
+        assert_eq!(
+            DosHeader2::from(h),
+            DosHeader2 {
+                magic: 0x5A4D,
+                cblp: 0x01D4,
+                cp: 0x0006,
+                crlc: 0x0000,
+                cparhdr: 0x0020,
+                minalloc: 0x0000,
+                maxalloc: 0xFFFF,
+                ss: 0x0000,
+                sp: 0x0000,
+                csum: 0x983D,
+                ip: 0x0000,
+                cs: 0x0000,
+                lfarlc: 0x0040,
+                ovno: 0x0000,
+                res: [0x0001, 0x0000, 0x0000, 0x0000],
+                oemid: 0x1000,
+                oeminfo: 0x0000,
+                res2: [
+                    0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x1000, 0x0000, 0x0000, 0x0000,
+                ],
+                lfanew: 0x0600,
+            }
+        );
+    }
+}
